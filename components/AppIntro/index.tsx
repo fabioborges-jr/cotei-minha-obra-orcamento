793881dcb828ec860c185179679184c64303d665
Fabio <fabioborgesouzajunior@gmail.com>
2023-09-19 23:01:12 -0300

    editing AppIntro, insert svg logotype, installing next auth

---
+++ components/AppIntro/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function AppIntro() {
+  return (
+    <section>
+      <div>
+        <Image src="/cotei-logo.svg" alt="logo" width={82} height={65}></Image>
+        <h1>Bem-vindo (a)</h1>
+        <p>
+          Tome decisões informadas com dados confiáveis <br />
+          <strong>(SINAPI, SETOP, ENTRE OUTRAS)</strong> e estimativas <br />
+          precisas. Simplifique seu trabalho na construção.
+        </p>
+      </div>
+    </section>
+  )
+}
