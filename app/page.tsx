5892d0ca531483796bc317dda4a94f74d3639812
Fabio <fabioborgesouzajunior@gmail.com>
2023-09-22 00:45:29 -0300

    adding authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import LoginButton from '@/components/LoginButton/intex'
 import AppIntro from '../components/AppIntro'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth/next'
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       <AppIntro />
+      <LoginButton />
     </main>
   )
 }
