aa3dbd6626461e933902acc289974060adf6b197
Fabio <fabioborgesouzajunior@gmail.com>
2023-10-05 19:45:39 -0300

    adding project router

--- components/ProjectButton/index.tsx
+++ components/ProjectButton/index.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { PiPlus } from 'react-icons/pi'
 import { BsFillTrash3Fill } from 'react-icons/bs'
+import Link from 'next/link'
 
 type ButtonProps = {
   project: {
@@ -29,13 +29,18 @@ export default function ProjectButton(props: ButtonProps) {
     })
   }
   return (
-    <button className="relative bg-color2 rounded-sm w-1/5 flex flex-col items-center justify-center">
-      {props.project.name}
-      <BsFillTrash3Fill
-        onClick={handleDeleteProject}
-        className="absolute right-0 bottom-0 mb-3 mr-3"
-        size={24}
-      />
-    </button>
+    <Link
+      href="/project"
+      className="relative bg-color2 rounded-sm w-1/5 flex flex-col items-center justify-center"
+    >
+      <button className="">
+        {props.project.name}
+        <BsFillTrash3Fill
+          onClick={handleDeleteProject}
+          className="absolute right-0 bottom-0 mb-3 mr-3"
+          size={24}
+        />
+      </button>
+    </Link>
   )
 }
