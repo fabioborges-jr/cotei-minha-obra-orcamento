dfbaa5c00047df8deed41784e25178a916182d44
Fabio <fabioborgesouzajunior@gmail.com>
2023-10-08 09:41:30 -0300

    onChange events select tag

--- components/AddCodeModal/index.tsx
+++ components/AddCodeModal/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 export default function AddCodeModal() {
   const [modalIsOpen, setIsOpen] = useState(false)
+  const [selectCode, setSelectCode] = useState(true)
   function openModal() {
     setIsOpen(true)
   }
@@ -15,16 +16,63 @@ export default function AddCodeModal() {
   function closeModal() {
     setIsOpen(false)
   }
-  return (
-    <>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-      ></Modal>
-      <button className="bg-color2 w-5/6" onClick={openModal}>
-        NOVO CÓDIGO OU SUBGRUPO
-      </button>
-    </>
-  )
+  function handleSelect(e: React.ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value === 'code') setSelectCode(true)
+    if (e.target.value === 'sub') setSelectCode(false)
+  }
+  if (selectCode) {
+    return (
+      <>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={{
+            overlay: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+          }}
+          className="bg-color2 h-1/5 w-1/5 place-content-center rounded-sm flex flex-col items-center p-10"
+        >
+          <select onChange={handleSelect}>
+            <option value="code">NOVO CÓDIGO</option>
+            <option value="sub">NOVO SUBGRUPO</option>
+          </select>
+          <h1>CONSULTE UMA COMPOSIÇÃO </h1>
+        </Modal>
+        <button className="bg-color2 w-5/6" onClick={openModal}>
+          NOVO CÓDIGO OU SUBGRUPO
+        </button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={{
+            overlay: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+          }}
+          className="bg-color2 h-1/5 w-1/5 place-content-center rounded-sm flex flex-col items-center p-10"
+        >
+          <select onChange={handleSelect}>
+            <option value="code">NOVO CÓDIGO</option>
+            <option value="sub">NOVO SUBGRUPO</option>
+          </select>
+          <h1>CONSULTE UMA COMPOSIÇÃO </h1>
+        </Modal>
+        <button className="bg-color2 w-5/6" onClick={openModal}>
+          NOVO CÓDIGO OU SUBGRUPO
+        </button>
+      </>
+    )
+  }
 }
