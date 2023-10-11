3c906d508886e4658105a506969c7b0aee57a619
Fabio <fabioborgesouzajunior@gmail.com>
2023-10-11 14:29:30 -0300

    testing without credentials provicer for production

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,25 +9,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: '' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        const user = {
-          id: '1',
-          email: 'cliente@exemplo.com',
-          password: '123456',
-        }
-        if (user) {
-          return user
-        } else {
-          return null
-        }
-      },
-    }),
   ],
 }
 
