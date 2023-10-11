968bec2a78ceda916ba259bae9075db420103407
Fabio <fabioborgesouzajunior@gmail.com>
2023-10-11 14:50:48 -0300

    adding providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,26 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, // teste
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: '' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: '1',
+          email: 'cliente@exemplo.com',
+          password: '123456',
+        }
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      },
     }),
   ],
 }
